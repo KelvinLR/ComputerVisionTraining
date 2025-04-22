78360fc82d3542375afe859503b0d476612119f6
Kelvin <kelvindelima710@gmail.com>
2025-04-22 11:20:17 -0300

    feat: finishing exercise 04

--- c++/ex04/ex04.cpp
+++ c++/ex04/ex04.cpp
@@ -3,6 +3,25 @@
 
 using namespace cv;
 
+Mat getColoredChannel(const Mat& image, int channelIndex) {
+    // criando array de canais
+    // e extraindo o canal desejado
+    // e preenchendo os outros canais com zeros
+    std::vector<Mat> zeros(3, Mat::zeros(image.size(), CV_8UC1));
+    Mat extracted;
+
+    // extraindo canal e copiando para o vetor de canais
+    // e preenchendo os outros canais com zeros
+    // o canal desejado é o canalIndex
+    // e os outros canais são preenchidos com zeros
+    extractChannel(image, extracted, channelIndex);
+    zeros[channelIndex] = extracted;
+    Mat result;
+    merge(zeros, result);
+
+    return result;
+}
+
 int main(int argc, char** argv) {
     if (argc != 2) {
         std::cout << "usage: DisplayImage.out <Image_Path>\n";
@@ -20,17 +39,50 @@ int main(int argc, char** argv) {
 
     std::cout << "Imagem carregada com sucesso!\n";
     
-    cv::cvtColor(image, hsv, COLOR_BGR2HSV); //image representa a Mat de origem, grey a de destino, e o parâmetro final é como a imagem será copiada
+    cv::cvtColor(image, hsv, COLOR_BGR2HSV); //image representa a Mat de origem, hsv a de destino, e o parâmetro final é como a imagem será copiada
     std::cout << "Dimensões: " << image.cols << " x " << image.rows << std::endl;
 
     namedWindow("Display Image", WINDOW_AUTOSIZE);
-    imshow("Display Image", hsv);
+    imshow("Display Image", hsv); 
+    // numa imagem HSV os canais não representam cores 
+    // mas atributos como matiz [h], saturação [s] e valor [v]
 
-    int k = waitKey(0);
+    int k;
 
-    if(k == 's') {
-        cv::imwrite("hsvausimage.png", hsv);
-    }
+    do {
+        std::cout << "\n[h] hue | [s] saturation | [v] value | [u] sair\n";
+        k = waitKey(0);
 
+        Mat canalColorido;
+        std::string nome;
+
+        switch(k) {
+            case 'h':
+                canalColorido = getColoredChannel(hsv, 2); // H
+                imshow("Display Image", canalColorido);
+                imwrite("ausHchannel.png", canalColorido);
+                std::cout << "Imagem salva como ausRchannel.png\n";
+                break;
+            case 's':
+                canalColorido = getColoredChannel(hsv, 1); // S
+                imshow("Display Image", canalColorido);
+                imwrite("ausSchannel.png", canalColorido);
+                std::cout << "Imagem salva como ausGchannel.png\n";
+                break;
+            case 'v':
+                canalColorido = getColoredChannel(hsv, 0); // V
+                imshow("Display Image", canalColorido);
+                imwrite("ausVchannel.png", canalColorido);
+                std::cout << "Imagem salva como ausBchannel.png\n";
+                break;
+            default:
+                imwrite("greyaus02.png", hsv);
+                std::cout << "Imagem original salva como greyaus02.png\n";
+                break;
+        }
+
+    } while (k != 'u');
+
+    // waitKey(0);
     return 0;
 }
