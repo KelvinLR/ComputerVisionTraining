6ce27dcb09e0f023e3520ae8083e8e7d4e84e5ff
Kelvin <kelvindelima710@gmail.com>
2025-04-30 11:35:16 -0300

    feat: finishing ex11

--- c++/ex11/ex11.cpp
+++ c++/ex11/ex11.cpp
@@ -11,9 +11,7 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-    cv::Mat image, gray;
-    std::ofstream myFile;
-    myFile.open("matrix.txt");
+    cv::Mat image, gray, thr;
 
     image = cv::imread(argv[1], IMREAD_COLOR);
 
@@ -23,57 +21,24 @@ int main(int argc, char** argv) {
     }
 
     std::cout << "Imagem carregada com sucesso!\n";
-
-    cv::Vec<unsigned char, 3> matrix[image.rows][image.cols];
-    
-    std::vector<cv::Mat> images;
-
-    for(int limiar = 0; limiar < 128; limiar++) {
-        cv::Mat imagemLimiarizada(image.rows, image.cols, CV_8UC1);
-        for(int i = 0; i < image.rows; i++) {
-            for(int j = 0; j < image.cols; j++) {
-                matrix[i][j] = image.at<cv::Vec3b>(i, j);
-                cv::Vec3b pixel = matrix[i][j];
-                int R = pixel[2];
-                int G = pixel[1];
-                int B = pixel[0];
-        
-                int media = (R + G + B) / 3;
-        
-                if (media > limiar) {
-                    imagemLimiarizada.at<uchar>(i, j) = 255;
-                } else {
-                    imagemLimiarizada.at<uchar>(i, j) = 0;
-                }
-            }
-        }
-        images.push_back(imagemLimiarizada);
-    }
     
     cv::cvtColor(image, gray, COLOR_BGR2GRAY); 
+    cv::threshold(gray, thr, 100,255,THRESH_BINARY);
+
     std::cout << "Dimensões: " << image.cols << " x " << image.rows << std::endl;
 
-    for(int i = 0; i < image.rows; i++) {
-        for(int j = 0; j < image.cols; j++) {
-            myFile << "pixel [" << i << "][" << j << "] = " << (int)matrix[i][j][0] << " " << (int)matrix[i][j][1] << " " << (int)matrix[i][j][2];
-            std::cout << matrix[i][j] << " ";
-        }
-    }
-   
-    myFile.close();
-    
-    cv::namedWindow("Limiarizacao", cv::WINDOW_AUTOSIZE);
-    while (true) {
-        for (int i = 0; i < images.size(); i++) {
-            cv::imshow("Limiarizacao", images[i]);
-            if (cv::waitKey(50) == 27) 
-                return 0;
-        }
-    }
-    cv::destroyAllWindows();
+    Moments m = moments(thr,true);
+    Point p(m.m10/m.m00, m.m01/m.m00);
 
-    int k;
-    k = waitKey(0);
+    circle(image, p, 5, Scalar(128,0,0), -1);
+    namedWindow("Display Image", WINDOW_AUTOSIZE);
+    imshow("Display Image", image);
+
+    int k = waitKey(0);
+
+    if(k == 's') {
+        cv::imwrite("greyaus02.png", thr);
+    }
 
     return 0;
 }
