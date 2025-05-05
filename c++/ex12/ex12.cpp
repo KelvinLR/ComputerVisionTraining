ac2087caa936121b788f5d9e749f941907567019
Kelvin <kelvindelima710@gmail.com>
2025-05-05 16:39:17 -0300

    feat: finishing exercise 12

--- c++/ex12/ex12.cpp
+++ c++/ex12/ex12.cpp
@@ -6,34 +6,51 @@
 using namespace std;
 
 int main() {
-
-    ifstream file("matrix.txt");
+    fstream file("matrix.txt");
     string line;
+    int cols = 0, rows = 0;
 
     if (!file.is_open()) {
-        std::cout << "Erro ao abrir o arquivo.\n";
+        cout << "Erro ao abrir o arquivo.\n";
         return -1;
     }
 
     while (std::getline(file, line)) {
-        cout << line << std::endl;
+        if (rows == 0) {
+            std::istringstream ss(line);
+            int value, count = 0;
+            while (ss >> value) count++;
+            cols = count / 3;
+        }
+        rows++;
+    }
+
+    cout << rows << "x" << cols << std::endl;
+
+    cv::Mat image(rows, cols, CV_8UC3);
+
+    file.clear();
+    file.seekg(0, ios::beg);
+    int row = 0;
+
+    while (std::getline(file, line) && row < rows) {
+        std::istringstream ss(line);
+        int r, g, b;
+        int col = 0;
+
+        while (col < cols && ss >> r >> g >> b) {
+            image.at<cv::Vec3b>(row, col) = cv::Vec3b(b, g, r);
+            col++;
+        }
+
+        row++;
     }
 
     file.close();
 
-    cv::Mat image;
-    namedWindow("Display Image", WINDOW_AUTOSIZE);
-    imshow("Display Image", image);
-
-    int k = waitKey(0);
-
-    if(k == 's') {
-        cv::imwrite("beachImage.png", image);
-    }
-
-    
-
-   
+    cv::imshow("Imagem", image);
+    cv::waitKey(0);
+    cv::imwrite("beachImage.png", image); 
 
     return 0;
 }
