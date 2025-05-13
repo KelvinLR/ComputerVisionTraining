ffd7ad8df7e94494014a1efaede91811b9e95eef
Kelvin <kelvindelima710@gmail.com>
2025-05-13 15:17:06 -0300

    feat: finishing ex13

--- c++/ex13/ex13.cpp
+++ c++/ex13/ex13.cpp
@@ -3,31 +3,44 @@
 #include <fstream>
 #define MAX_KERNEL_SIZE 30
 
-//using namespace std;
+using namespace std;
+using namespace cv;
 
 int main(int argc, char** argv) {
     if (argc != 2) {
-        std::cout << "usage: DisplayImage.out <Image_Path>\n";
+        cout << "usage: DisplayImage.out <Image_Path>\n";
         return -1;
     }
 
-    cv::Mat image;
+    Mat image;
+    Mat_<uchar> gray_image;
+
+    int Gx, Gy;
     
-    image = cv::imread(argv[1], cv::IMREAD_COLOR);
+    image = imread(argv[1], IMREAD_COLOR);
+    cvtColor(image, gray_image, COLOR_BGR2GRAY);
 
     if (!image.data) {
-        std::cout << "Não foi possível abrir ou encontrar a imagem\n";
+        cout << "Não foi possível abrir ou encontrar a imagem\n";
         return -1;
     }
 
-    std::cout << "Imagem carregada com sucesso!\n";
+    cout << "Imagem carregada com sucesso!\n";
 
-    cv::Vec<unsigned char, 3> matrix[image.rows][image.cols];
+    Mat_<uchar> sobel_filter(gray_image.rows, gray_image.cols, 1);
 
-    std::cout << matrix;
-    cv::imshow("Imagem", image);
-    cv::waitKey(0);
-    cv::imwrite("beachImage.png", image); 
+    for (int y = 1; y < gray_image.rows - 1; y++) {
+        for (int x = 1; x < gray_image.cols - 1; x++) {
+            Gx = (1)*gray_image.at<uchar>(y-1,x-1) + (2)*gray_image.at<uchar>(y-1,x) + (1)*gray_image.at<uchar>(y-1,x+1) + (-1)*gray_image.at<uchar>(y+1,x-1) + (-2)*gray_image.at<uchar>(y+1,x) + (-1)*gray_image.at<uchar>(y+1,x+1);
+            Gy = (1)*gray_image.at<uchar>(y-1,x-1) + (-1)*gray_image.at<uchar>(y-1,x+1) + (2)*gray_image.at<uchar>(y,x-1) + (-2)*gray_image.at<uchar>(y,x+1) + (1)*gray_image.at<uchar>(y+1,x-1) + (-1)*gray_image.at<uchar>(y+1,x+1); 
+
+            sobel_filter.at<uchar>(y,x) = (int)sqrt(pow(Gx, 2) + pow(Gy, 2));
+        }
+    }
+    
+    imshow("Imagem", sobel_filter);
+    waitKey(0);
+    imwrite("aussieSobel.png", image); 
 
     return 0;
 }
